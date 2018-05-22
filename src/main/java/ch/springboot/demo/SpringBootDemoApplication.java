c7c82dc5c702c030cc98550c6b84a43cf0319db5
chaohong.wang <chaohong.wang@qishon.com>
2018-05-22 10:56:05 +0800

    基础代码

---
+++ src/main/java/ch/springboot/demo/SpringBootDemoApplication.java
@@ -0,0 +1,16 @@
+package ch.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  enter
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class);
+    }
+
+}
---
+++ src/main/java/ch/springboot/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package ch.springboot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping(value = {"/","/index"})
+    public String index(){
+        return "index";
+    }
+}
